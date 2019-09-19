72d527331db10e99f33e488ceb99cffa258a82da
M.Vlasova <m.vlasova@niias.ru>
2019-09-19 11:39:29 +0300

    from html to jsx code

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const itemsData = require('./products.json');
+
+function App() {
+  const data = itemsData;
+  return (
+    <div className="container">
+      <div className="container_list">
+        <h1>Список товаров</h1>
+        <ul>
+          {
+            data.filter(item => item.id < 4).map(item => <li key={item.id}>{item.name}</li>)
+          }
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
